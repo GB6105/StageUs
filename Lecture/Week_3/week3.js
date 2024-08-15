8461286ab861afacb2d2bff03cdd9ce3ad26112d
gb6105 <xorbskim6105@gmail.com>
2024-08-15 18:37:58 +0900

    new task version added

--- Lecture/Week_3/week3.js
+++ Lecture/Week_3/week3.js
@@ -9,7 +9,7 @@ printMessage() //함수 호출
 volumeOfSphere()
 printMessage()
 
-매개 변수return 값 둘 다 없는 경우
+//매개 변수return 값 둘 다 없는 경우
 function printMessage(){
     console.log("Hello")
 }
@@ -38,11 +38,11 @@ function add(number1, number2){
 
 console.log(add(1,3))
 
-변수
+//변수
 
-변수는 뭔가를 담아두는 공간
-변수를 만들면. RAM에 만들어짐
-공간할당
+//변수는 뭔가를 담아두는 공간
+//변수를 만들면. RAM에 만들어짐
+//공간할당
 
 var number = 10
 
@@ -66,7 +66,7 @@ var value3 = 10.0   //double
 console.log(toString(value1) + toString(value1))    //20
 console.log(parseInt(value2) + parseInt(value2))  //1010 // parseInt 정수형으로 바꿔주는 함수
 
-전역 변수
+//전역 변수
 var number =10  //전역 변수 !!메모리가 낭비되므로 잦은 사용 주의
                 // 메모리적 측면에서 접근
 
@@ -101,16 +101,16 @@ function addEvent(){
     document.getElementById("dest").innerHTML= number1 + number2
 }
 
-목표: 더하기 기능 만들기 탑다운 방식
-1. 레이아웃 만들기
+// 목표: 더하기 기능 만들기 탑다운 방식
+// 1. 레이아웃 만들기
 
-2. 입력한 숫자 가져와 저장하기
+// 2. 입력한 숫자 가져와 저장하기
 
-3. 더하기
+// 3. 더하기
 
-4. 화면에 출력하기
+// 4. 화면에 출력하기
 
-빼기 곱하기 나누기
+// 빼기 곱하기 나누기
 
 function addEvent(){
     var number1 = parseInt(document.getElementById("number1").value)
