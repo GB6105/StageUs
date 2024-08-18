c6dd22c20908171ad9c32abf0b3f67630881d668
GB <xorbskim6105@gmail.com>
2024-08-19 01:17:17 +0900

    fiexd

--- Lecture/Week_3/BaseballGame.js
+++ Lecture/Week_3/BaseballGame.js
@@ -1,89 +1,126 @@
 // 함수 생성 본
 // function getRandomVar(){
-//     var random1= Math.floor(Math.random() * 10);
-//     var random2= Math.floor(Math.random() * 10);
-//     var random3= Math.floor(Math.random() * 10);
-//     if (random1 == random2 || random1 == random3 || random2 == random3){
+//     var target1= Math.floor(Math.random() * 10);
+//     var target2= Math.floor(Math.random() * 10);
+//     var target3= Math.floor(Math.random() * 10);
+//     if (target1 == target2 || target1 == target3 || target2 == target3){
 //         getRandomVar()
 //     }
-//     console.log(random1)
-//     console.log(random2)
-//     console.log(random3)
+//     console.log(target1)
+//     console.log(target2)
+//     console.log(target3)
 // }
 // getRandomVar()
 
-var t1=0, t2=0, t3=0
-var arr = [];
+// var t1=0, t2=0, t3=0
+// var arr = [];
 
-function getRandomVar(){
-    var random1= Math.floor(Math.random() * 10);
-    var random2= Math.floor(Math.random() * 10);
+// function getRandomVar(){
+//     var target1= Math.floor(Math.random() * 10);
+//     var target2= Math.floor(Math.random() * 10);
     
-    var random3= Math.floor(Math.random() * 10);
-    if (random1 == random2 || random1 == random3 || random2 == random3){
-        getRandomVar()
+//     var target3= Math.floor(Math.random() * 10);
+//     if (target1 == target2 || target1 == target3 || target2 == target3){
+//         getRandomVar()
+//     }
+//     // console.log(target1)
+//     // console.log(target2)
+//     // console.log(target3)
+
+//     document.getElementById("target1").innerHTML = target1;
+//     document.getElementById("target2").innerHTML = target2;
+//     document.getElementById("target3").innerHTML = target3;
+
+// }
+
+var target1, target2, target3;
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * 10)
+}
+
+function getTargetNumber(){
+    target1= getRandomNumber()
+    target2 = getRandomNumber()
+    if (target1 == target2){
+        target2 = getRandomNumber()
     }
-    // console.log(random1)
-    // console.log(random2)
-    // console.log(random3)
+    target3 = getRandomNumber()
+    if (target1 == target3|| target2 == target3){
+        target3 = getRandomNumber()
+    }
 
-    document.getElementById("target1").innerHTML = random1;
-    document.getElementById("target2").innerHTML = random2;
-    document.getElementById("target3").innerHTML = random3;
+    document.getElementById("target1").innerHTML = target1
+    document.getElementById("target2").innerHTML = target2
+    document.getElementById("target3").innerHTML = target3
 
+    console.log("Answer is ",target1, target2,target3)
 }
 
 
+function judge(target1,target2,target3){
 
-var input1 = parseInt(document.getElementById("number1").value);
-var input2 = parseInt(document.getElementById("number2").value);
-var input3 = parseInt(document.getElementById("number3").value);
-console.log(input1)
-console.log(input2)
-console.log(input3)
-
-
-function judge(random1,random2,random3){
+    var input1 = parseInt(document.getElementById("number1").value);
+    var input2 = parseInt(document.getElementById("number2").value);
+    var input3 = parseInt(document.getElementById("number3").value);
+    
     var strike = 0;
     var ball = 0;
 
-    if(input1 == random1){
+    //1번 숫자 판정
+    if(input1 == target1){
         strike += 1
     }
-    else if (input1 == random2 || input2 == random3){
+    else if (input1 == target2 || input2 == target3){
         ball += 1
     }
     
-
-    if (input2 == random2){
+    //2번 숫자 판정
+    if (input2 == target2){
         strike += 1
     }
-    else if (input2 == random1 || input2 == random3){
+    else if (input2 == target1 || input2 == target3){
         ball += 1
     }
     
-    if (input3 == random3){
+    //3번 숫자 판정
+    if (input3 == target3){
         strike += 1
     }
-    else if(input3 == random1 || input3 == random2){
+    else if(input3 == target1 || input3 == target2){
         ball += 1
     }
 
     console.log(strike)
     console.log(ball)
 
-    document.getElementById("strike").innerHTML = strike;
-    document.getElementById("ball").innerHTML = ball;
+    if (strike == 3){
+        document.getElementById("정답").innerHTML = "정답입니다."
+    }
+    else{
+        document.getElementById("정답").innerHTML = "다시 시도해보세요"
+    }
+
+
+    document.getElementById("strike").innerHTML = strike+" Strike";
+    document.getElementById("ball").innerHTML = ball+" Ball";
+
+    return strike;
+    
+}
+
+function atari(){
+    if (strike == 3){
+        document.getElementById("정답").innerHTML = "정답입니다."
+    }
 
 }
 
-
-getRandomVar()
-
+atari()
 
 
+// function printMessage(){
+//     console.log("hello")
+// }
+// printMessage()
 
-function printMessage(){
-    console.log("hello")
-}
-printMessage()
