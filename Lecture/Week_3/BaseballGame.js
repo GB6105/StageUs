472d6b51286b566e3378368041d0445d742a9cfa
gb6105 <xorbskim6105@gmail.com>
2024-08-21 19:22:23 +0900

    feat/minorUpdate

--- Lecture/Week_3/BaseballGame.js
+++ Lecture/Week_3/BaseballGame.js
@@ -121,7 +121,7 @@ function judge(target1,target2,target3){
         //상황출력 판정 부분
         if (strike == 3){ // 3개 모두 맞출 경우 상황출력 처리
             //결과판 출력
-            document.getElementById("상황출력").innerHTML = "상황출력입니다."
+            document.getElementById("상황출력").innerHTML = "맞췄습니다!"
             document.getElementById("스트라이크").innerHTML = "3 Strike"
             //전광판 출력
             document.getElementById("strike1").style.backgroundColor = "yellow";
@@ -134,13 +134,13 @@ function judge(target1,target2,target3){
                 out += 1
                 document.getElementById("out1").style.backgroundColor = "red";
                 getTargetNumber(); //다음게임으로
-                resetInputVlaue();
+                resetInputValue();
             }
             else if(out == 1){ //2out 상황
                 out += 1
                 document.getElementById("out2").style.backgroundColor = "red";
                 getTargetNumber();//다음게임으로
-                resetInputVlaue();
+                resetInputValue();
 
             }
             else if(out == 2){ //3out으로
@@ -209,7 +209,7 @@ function judge(target1,target2,target3){
             input6 = input3;
             strike2 = strike;
             ball2 = ball;
-            resetInputVlaue(); //입력된 값 초기화         
+            resetInputValue(); //입력된 값 초기화         
         }
         resetInputBox(); //숫자 입력 박스 초기화
         resetScore();   //점수 초기화
@@ -225,7 +225,7 @@ var blinckOut1Terminal;
 var blinckOut2Terminal;
 
 // 입력,상황출력,전광판,게임 등 리셋하는 함수 부분/////////////////////////////////////////
-function resetInputVlaue(){ //입력값 초기화
+function resetInputValue(){ //입력값 초기화
     input1 = 0;
     input2 = 0;
     input3 = 0;
@@ -287,7 +287,7 @@ function newInput(){    //사용자가 직접 입력을 다시 할 수 있음
 }
 function resetGame(){   //게임 재시작(개발용) 추후 새게임에 기능 합치기
     resetTarget();
-    resetInputVlaue();
+    resetInputValue();
     resetScoreBoard();
     // document.getElementById("상황출력").innerHTML = "숫자를 입력하세요"
     //getTargetNumber(); // 이게 게임 시작의 핵심 하드리셋으로 옮기면 
@@ -297,7 +297,9 @@ function resetGame(){   //게임 재시작(개발용) 추후 새게임에 기능
 }
 
 function hardResetGame(){   //처음부터 다시시작
-    resetGame();
+    resetTarget();
+    resetInputValue();
+    resetScoreBoard();
     resetLastInputBox();
    
     out = 0;
@@ -359,6 +361,8 @@ function judgeNumber(inputValue,t1,t2,t3,k){
 // 반투명 팝업창으로 전체화면 덥고 해당 파트에 대한 설명 작성해주기
 
 
+
+
 // 튜토리얼
 // 버튼 눌러서 시작
 // 변수는 임의로 지정해주기
@@ -368,8 +372,28 @@ function judgeNumber(inputValue,t1,t2,t3,k){
 // ball에 대한 설명 , strike에 대한설명
 // 정답 기준에 대한 설명 
 
+function tutorial(){
+    //안내문구 작성
+    document.getElementById("상황출력").innerHTML = "튜토리얼을 시작합니다."
+    target1 = 8;
+    target2 = 3;
+    target3 = 7;
+    printPart1();
+    
 
-
+    // if(ball2 == 1){
+    //     printPart2();
+    // }
+    // else{
+    //     document.getElementById("상황출력").innerHTML = "1 2 3 을순서대로 입력해주세요"
+    // }
+    // printPart2();
+    // printPart3();
+    // printPart4();
+    // printPart5();
+    // printPart6();
+    
+}
 
 
 //추가적인 잡다한 기능
@@ -465,3 +489,111 @@ function glowOutOutSign2(){
     document.getElementById("out2").style.backgroundColor = 'gray'
 }
 
+
+// 대사 뭉치
+function text1(){
+    document.getElementById("상황출력").innerHTML = "MLB는 컴퓨터가 생각한 임의의 숫자를 유추하는 게임입니다."
+}
+
+function text2(){
+    document.getElementById("상황출력").innerHTML = "지금 컴퓨터가 생각한 값은 8 3 7입니다."
+}
+
+function text3(){
+    document.getElementById("상황출력").innerHTML = "숫자를 입력하면 숫자가 있는지 없는지를 알 수 있습니다."
+}
+
+function text4(){
+    document.getElementById("상황출력").innerHTML = "1 2 3을 입력해보세요"
+}
+function printPart1(){
+    setTimeout(text1, 2000)
+    setTimeout(text2, 4000)
+    setTimeout(text3, 6000)
+    setTimeout(text4, 8000)
+}
+
+//PART2
+function text5(){
+    document.getElementById("상황출력").innerHTML = "정답에 3이 있지만 순서가 맞지 않았습니다." 
+}
+
+function text6(){
+    document.getElementById("상황출력").innerHTML = "이럴 경우 1개의 숫자가 맞았지만 순서가 틀렸으므로 1 ball입니다."
+} 
+
+function text7(){
+    document.getElementById("상황출력").innerHTML = "1 3 5를 입력해보세요"
+}
+
+function printPart2(){
+    setTimeout(text5,2000)
+    setTimeout(text6,4000)
+    setTimeout(text7,6000)
+}
+
+//PART3
+function text8(){
+    document.getElementById("상황출력").innerHTML = "3이 순서까지 맞았으므로 1 strike 입니다." 
+}
+
+function text9(){
+    document.getElementById("상황출력").innerHTML = "7 3 5 을 입력해보세요"
+}
+function printPart3(){
+    setTimeout(text8,2000)
+    setTimeout(text9,4000)
+}
+
+//PART4
+function text10(){
+    document.getElementById("상황출력").innerHTML = "결과는 1stike 1ball 입니다. 3이 stike라는 것을 알 수 있었으므로"
+}
+
+function text11(){
+    document.getElementById("상황출력").innerHTML = "새롭게 입력한 7이 들어간 다는 것을 유추할 수 있습니다." 
+}
+
+function text12(){
+    document.getElementById("상황출력").innerHTML = "5 3 7 을 입력해보세요"
+}
+function printPart4(){
+    setTimeout(text10,2000)
+    setTimeout(text11,4000)
+    setTimeout(text12,6000)
+}
+
+//PART5
+function text13(){
+    document.getElementById("상황출력").innerHTML = "결과는 2stike 입니다. 이제 남은 한 자리만 맞추면 되겠네요"
+}
+
+function text14(){
+    document.getElementById("상황출력").innerHTML = "정답을 입력해보세요"
+}
+
+function text15(){
+    document.getElementById("상황출력").innerHTML = "이제 8 3 7 을 입력해보세요"
+}
+function printPart5(){
+    setTimeout(text13,2000)
+    setTimeout(text14,4000)
+    setTimeout(text15,6000)
+}
+//PART6
+function text16(){
+    document.getElementById("상황출력").innerHTML = "숫자 3개와 순서까지 맞추었으므로 3strike 입니다."
+}
+
+function text17(){
+    document.getElementById("상황출력").innerHTML = "정답을 맞추면 다음 게임으로 넘어갑니다."
+}
+
+function text18(){
+    document.getElementById("상황출력").innerHTML = "총 3번 정답을 맞추면 이깁니다. 행운을 빕니다."
+}
+function printPart6(){
+    setTimeout(text16,2000)
+    setTimeout(text17,4000)
+    setTimeout(text18,6000)
+}
