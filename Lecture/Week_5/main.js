7ea93c70cfc1ef0ea7dd3f3b4e92133fa0380293
gb6105 <xorbskim6105@gmail.com>
2024-09-03 19:26:33 +0900

    background video imported and minor update

---
+++ Lecture/Week_5/main.js
@@ -0,0 +1,18 @@
+// function hideCategoryBox() {
+//     document.getElementById("weeklyTask").style.display =""
+// }
+
+// function showCategoryBox(e) {
+//     document.getElementById("weeklyTask").style.display ="none"
+// }
+
+// function hideCategoryBox() {
+//     document.getElementById("weeklyTask").style.visibility ="hidden"
+// }
+
+// function showCategoryBox(e) {
+//     document.getElementById("weeklyTask").style.visibility =""
+// }
+
+
+// hideCategoryBox();
