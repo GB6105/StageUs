4c1d8fa2bf70c07a24e6a41aa1cbf01e3ac6c2b1
GB <xorbskim6105@gmail.com>
2024-09-06 05:11:58 +0900

    week4/organize

--- Lecture/Week_4/Feedback/PuzzleBuzzleMain.js
+++ Lecture/Week_4/Feedback/PuzzleBuzzleMain.js
@@ -1,7 +1,8 @@
-var level;
+
+var draggedImgSrc;
 
 function Level(n){
-    level = n;
+    var level = n;
     makePuzzleZone(level,"startZone");
     importImage(level);
     makePuzzleZone(level,"answerZone");
@@ -16,9 +17,11 @@ function makePuzzleZone(level,direction){
     for (var i = 1; i < level**2  + 1 ; i++ ){
         var puzzleBoard = document.createElement("div");
         puzzleBoard.id = i;
-        puzzleBoard.draggable = "true";
         puzzleBoard.className = "puzzleBoard"
+        puzzleBoard.draggable = "true";
         puzzleBoard.style = "width:" + (90/level)+"%; height:" + (90/level)+"%;"
+        puzzleBoard.ondrag = dragOver
+        puzzleBoard.ondragend = setImgSrc
         document.getElementById(direction).appendChild(puzzleBoard);
     }
 }
@@ -32,7 +35,34 @@ function importImage(level){
 
     for (var i = 0; i < arr.length; i++){
         var imageSource = document.createElement("img");
+        imageSource.id = "img" + arr[i];
         imageSource.src = "images/level" + level + "/sampleImg_" + arr[i] +".jpg";
+        imageSource.draggable = "true";
+        imageSource.ondragstart = getImgSrc;
+        imageSource.ondrag = dragOver;
+        imageSource.ondragend = setImgSrc;
         document.getElementById(i+1).appendChild(imageSource);
     }
 }
+
+
+
+function getImgSrc(event){
+    var draggedImgSrcId = event.target.id;
+    // draggedImgSrc = document.getElementsByTagName("img");
+    draggedImgSrc = document.getElementById(draggedImgSrcId).src;
+    // console.log(draggedImgSrcId)
+    console.log(draggedImgSrc)
+}
+
+function dragOver(event){
+    event.preventDefault()
+}
+
+function setImgSrc(event){
+    if(,   )
+    
+
+
+}
+
